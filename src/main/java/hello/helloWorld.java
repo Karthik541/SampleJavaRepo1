e1f2af4627411a65cbe100893d9ad7ca49abb51f
training_B8C.03.01 <training_B8C.03.01@accenture.com>
2017-08-31 12:20:46 +0530

    Removed duplicate sysouts

--- src/main/java/hello/helloWorld.java
+++ src/main/java/hello/helloWorld.java
@@ -3,8 +3,5 @@ package hello;
 public class helloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-		System.out.println("Added newly");
-		System.out.println("Added newly");
-		System.out.println("Added newly");
     }
 }
